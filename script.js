80e0ba4dabc1d3e1c6096b768acc5c4cbaa575ad
Iryna Bilinska <bilinska.iryna.02@gmail.com>
2026-08-22 14:54:23 +0200

    Add completed task counter

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const taskInput = document.getElementById("taskInput");
 const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
+const counter = document.getElementById("counter");
+
+let completedTasks = 0;
+
+function updateCounter() {
+  counter.textContent = completedTasks;
+}
 
 addButton.addEventListener("click", () => {
   const taskText = taskInput.value.trim();
@@ -13,16 +20,32 @@ addButton.addEventListener("click", () => {
 
   task.innerHTML = `
         <label>
-            <input type="checkbox">
+            <input type="checkbox" class="complete-checkbox">
             ${taskText}
         </label>
         <button class="remove-button">Remove</button>
     `;
 
+  const checkbox = task.querySelector(".complete-checkbox");
   const removeButton = task.querySelector(".remove-button");
 
+  checkbox.addEventListener("change", () => {
+    if (checkbox.checked) {
+      completedTasks++;
+    } else {
+      completedTasks--;
+    }
+
+    updateCounter();
+  });
+
   removeButton.addEventListener("click", () => {
+    if (checkbox.checked) {
+      completedTasks--;
+    }
+
     task.remove();
+    updateCounter();
   });
 
   taskList.appendChild(task);
